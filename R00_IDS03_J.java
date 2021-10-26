1766f5cbbad8b81127484779aaae006b25d7535b
Nikolai Korolkov <54649431+RootIsHidden@users.noreply.github.com>
2021-10-25 21:50:25 -0400

    Added 10 Compliant examples.
    Added 10 compliant examples corresponding to 10 different rules.

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,10 +1,11 @@
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 // Rule 00. Input Validation and Data Sanitization (IDS)
 // IDS03-J. Do not log unsanitized user input
 // Refference: https://wiki.sei.cmu.edu/confluence/display/java/IDS03-J.+Do+not+log+unsanitized+user+input
 // =======================================================================================================
-// Non-Compliant Solution - allows for injection attack.
+// Compliant Solution - user input (username) is sanitized.
 
 public class R00_IDS03_J 
 {
@@ -12,15 +13,20 @@ public class R00_IDS03_J
     private static Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());;
     private static String username = "guest";
 
+    public static String sanitizeUser(String username) 
+    {
+        return Pattern.matches("[A-Za-z0-9_]+", username) ? username : "unauthorized user";
+    }
+
     public static void main(String [] args)
     {
         if (loginSuccessful) 
         {
-            logger.severe("User login succeeded for: " + username);
+            logger.severe("User login succeeded for: " + sanitizeUser(username));
         }
         else
         {
-            logger.severe("User login failed for: " + username);
+            logger.severe("User login failed for: " + sanitizeUser(username));
         }
     }   
 }
--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -4,7 +4,7 @@ import java.io.File;
 // XP00-J. Do not ignore values returned by methods
 // Refference: https://wiki.sei.cmu.edu/confluence/display/java/EXP00-J.+Do+not+ignore+values+returned+by+methods
 // ==============================================================================================================
-// Non-Compliant Solution - method fails to check if the operation has succeeded.
+// Compliant Solution - method checks if the operation has succeeded.
 
 public class R02_XP00_J 
 {
@@ -13,6 +13,9 @@ public class R02_XP00_J
         File someFile = new File("someFileName.txt");
         
         // Do something with someFile
-        someFile.delete();
+        if (!someFile.delete()) 
+        {
+          // Handle failure to delete the file
+        }
     }
 }
